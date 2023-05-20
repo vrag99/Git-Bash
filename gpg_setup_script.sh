b8d7692b9a51627bd02db7a092ebe1eccc102d7d
vrag99 <garv.codes@gmail.com>
2023-05-21 01:16:22 +0530

    updated gpg setup script

--- gpg_setup_script.sh
+++ gpg_setup_script.sh
@@ -14,6 +14,27 @@ gitConfig () {
     gpg --export --armor $1 | cat
     echo "Copy this and paste in your GitHub's gpg key section."
 }
+
+getLatestGPGKeyID () {
+    # Get the rsa id part which is preceeded by sec
+    keys=$(gpg --list-secret-keys --keyid-format=long | awk '/sec/{print $2}')
+
+    # Creating the array of those keys.
+    keyList=($(echo $keys))
+
+    # Getting the last key and splitting it by '/'
+    lastKey=($(echo ${keyList[-1]} | tr '/' ' '))
+
+    # The id is preceeded by a '/', hence getting the id of the latest gpg key.
+    latestGpgKeyId=${lastKey[1]}
+
+    echo $latestGpgKeyId
+}
+
+configureToLatestGPG () {
+    latestGPGKeyID=$(getLatestGPGKeyID)
+    gitConfig $latestGPGKeyID
+}
 #####################################################################
 
 
@@ -45,10 +66,7 @@ if [[ `gpg --list-keys` ]] ; then
     
     elif [ $choice -eq 2 ]; then
         gpg --full-generate-key
-        gpg --list-secret-keys --keyid-format=long
-        echo -n "Enter the secret key to be used: "
-        read key
-        gitConfig $key
+        configureToLatestGPG
 
     elif [ $choice -eq 3 ]; then
         gpg --list-secret-keys
@@ -63,4 +81,5 @@ else
     prettyPrint "It seems you don't have any gpg keys generated..
     Generating one for you :)"
     gpg --full-generate-key
+    configureToLatestGPG
 fi
